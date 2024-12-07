e48f1018ce6c0533af24568ca40d529cf123674a
manisharora <9manisharora@gmail.com>
2024-12-07 11:05:56 +0800

    Initial Commit

---
+++ src/test/java/com/hackathon/MortgageDemo/MortgageDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.MortgageDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MortgageDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
